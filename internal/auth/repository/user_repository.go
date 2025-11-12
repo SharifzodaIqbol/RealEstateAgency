c36c8f269c286ef0c20161ee2b2cef947fdf7014
Шарифзода Икбол <iqbolsharifzoda724@gmail.com>
2025-11-12 09:14:07 +0300

    Написал func FindUserByIdentifier для входа пользователя по его выбору либо username либо email

--- internal/auth/repository/user_repository.go
+++ internal/auth/repository/user_repository.go
@@ -4,6 +4,6 @@ import "auth-service/internal/auth/model"
 
 type UserRepository interface {
 	CreateUser(user *model.User) error
-	FindUserByEmail(email string) (*model.User, string, error)
 	FindRoleByName(roleName string) (int, error)
+	FindUserByIdentifier(identifier string) (*model.User, string, error)
 }
